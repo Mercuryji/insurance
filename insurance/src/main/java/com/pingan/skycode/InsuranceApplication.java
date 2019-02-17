7a9924b5d1ae8c782392677a54ed14812ed2b571
Mercuryji <mercuryji@126.com>
2019-02-17 22:12:26 +0800

    jicaiyun 02/17/2019

---
+++ insurance/src/main/java/com/pingan/skycode/InsuranceApplication.java
@@ -0,0 +1,17 @@
+package com.pingan.skycode;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("com.pingan.skycode.mapper")
+
+public class InsuranceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsuranceApplication.class, args);
+	}
+
+}
+
---
+++ insurance/src/main/java/com/pingan/skycode/controller/TestController.java
@@ -0,0 +1,37 @@
+package com.pingan.skycode.controller;
+
+import com.pingan.skycode.dfo.User;
+import com.pingan.skycode.service.TestService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * @ClassName: TestController
+ * @Description: TODO
+ * @Author: jicaiyun
+ * @Date: 21:35 2019/1/24
+ * @Version: V1.0.0
+ */
+@RestController
+@RequestMapping("/index")
+public class TestController {
+    @Autowired
+    private TestService testService;
+    @ApiOperation(value = "测试方法")
+    @GetMapping("/test")
+    public String test(@ApiParam(value = "入参",required = true)@RequestParam String param){
+        return param;
+    }
+    @ApiOperation(value = "测试mybatis")
+    @GetMapping("/mybatistest")
+    public List<User> mybatisTest(@ApiParam(value = "passport",required = true)@RequestParam String passport){
+        return testService.test(passport);
+    }
+}
